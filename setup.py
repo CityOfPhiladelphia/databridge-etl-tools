fbb278c9e98810be4298f4e99da539f32d30b70a
Victor Espinoza <vespinoza0391@gmail.com>
2021-05-21 12:05:52 -0400

    Install all new versions.

--- setup.py
+++ setup.py
@@ -7,31 +7,31 @@ setup(
     version='0.1.0',
     packages=['databridge_etl_tools',],
     install_requires=[
-        'boto3==1.9.137',
-        'botocore==1.12.137',
-        'certifi==2019.3.9',
-        'chardet==4.0.0',
-        'click==7.0',
-        'docutils==0.14',
-        'future==0.18.2',
-        'idna==2.10',
-        'jmespath==0.9.4',
-        'petl==1.2.0',
-        'pyrestcli==0.6.11',
-        'python-dateutil==2.8.1',
-        'requests==2.25.0',
-        's3transfer==0.2.0',
-        'six==1.16.0',
-        'urllib3==1.26.4'
+        'boto3',
+        'botocore',
+        'certifi',
+        'chardet',
+        'click',
+        'docutils',
+        'future',
+        'idna',
+        'jmespath',
+        'petl',
+        'pyrestcli',
+        'python-dateutil',
+        'requests',
+        's3transfer',
+        'six',
+        'urllib3'
     ],
     extras_require={
         'carto': ['carto'],
         'oracle': ['cx_Oracle'],
         'postgres': ['psycopg2-binary'],
         'dev': [
-            'moto==1.3.8',
-            'pytest==4.4.1',
-            'requests-mock==1.6.0'
+            'moto',
+            'pytest',
+            'requests-mock'
         ]
     },
     dependency_links=[
