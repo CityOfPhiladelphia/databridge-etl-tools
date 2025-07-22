60f22202ca96a02e4efa1831507d7c46048c2c00
Matthew Jackson <Matthew.Jackson@phila.gov>
2025-07-22 16:15:40 +0000

    start drafting tests

---
+++ tests/test_sharepoint.py
@@ -0,0 +1,35 @@
+import pytest 
+from databridge_etl_tools.sharepoint.sharepoint import Sharepoint
+from .constants import S3_BUCKET
+
+@pytest.fixture
+def sp_csv(graphapi_secret_name):
+    sharepoint_object = Sharepoint(
+        graphapi_secret_name=graphapi_secret_name,
+        site_name="ps360-metrics-share",
+        file_path="etl_tools_test_sheet.csv"
+        s3_bucket=S3_BUCKET,
+        s3_key="staging/test/sharepoint_csv_test.csv"
+    )
+    return sharepoint_object
+
+@pytest.fixture
+def sp_xlsx(graphapi_secret_name):
+    sharepoint_object = Sharepoint(
+        graphapi_secret_name=graphapi_secret_name,
+        site_name="ps360-metrics-share",
+        file_path="etl_tools_test_workbook.xlsx",
+        s3_bucket=S3_BUCKET,
+        s3_key="staging/test/sharepoint_xlsx_test.csv"
+        sheet_name="Dataset"
+    )
+    return sharepoint_object
+
+# TODO: define tests:
+    # - correct csv file name
+    # - correct xlsx file name and sheet name
+    # - correct xlsx file name, missing sheet name
+    # - correct xlsx file name, incorrect sheet name
+    # - preservation of headers
+    # - nonexistent/invalid file path
+    # - test that a csv with specific data loads the same info to S3 as a xlsx sheet with that same data
